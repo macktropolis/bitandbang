f8a1244e032ad0b257a26c82199e0930ceffcb81
Mack Richardson <mack@qube.consulting>
2022-08-05 22:20:43 -0400

    Added content

--- build.js
+++ build.js
@@ -1,59 +1,74 @@
-'use strict'
+"use strict";
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require("chalk");
+const boxen = require("boxen");
+const fs = require("fs");
+const path = require("path");
+const reduce = require("bluebird/js/release/reduce");
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
-}
+  borderStyle: "round",
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Staff Developer Advocate at Twilio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
-  labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
-  labelnpm: chalk.white.bold('     npm:'),
-  labelGitHub: chalk.white.bold('  GitHub:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
-  labelWeb: chalk.white.bold('     Web:'),
-  labelCard: chalk.white.bold('    Card:')
-}
+  name: chalk.white("   Mack Richardson"),
+  handle: chalk.white("macktropolis"),
+  shorthandle: chalk.white("mor"),
+  work: chalk.white("Web Developer at Qube Consulting"),
+  facebook: chalk.gray("https://facebook.com/") + chalk.cyan("macktropolis"),
+  // npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
+  github: chalk.gray("https://github.com/") + chalk.green("macktropolis"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("macktropolis"),
+  web: chalk.cyan("https://mackrichardson.com"),
+  codepen:
+    chalk.cyan("https://codepen.io") + " " + chalk.white("mackorichardson"),
+  npx: chalk.red("npx") + " " + chalk.gray("macktropolis"),
+  labelWork: chalk.white.bold("     Work:"),
+  labelFacebook: chalk.white.bold(" Facebook:"),
+  labelCodepen: chalk.white.bold("  CodePen:"),
+  labelGitHub: chalk.white.bold("   GitHub:"),
+  labelLinkedIn: chalk.white.bold(" LinkedIn:"),
+  labelWeb: chalk.white.bold("      Web:"),
+  labelCard: chalk.white.bold("     Card:"),
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = "\n";
+const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const facebooking = `${data.labelFacebook}  ${data.facebook}`;
+const penning = `${data.labelCodepen}  ${data.codepen}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output =
+  heading + // data.name + data.handle
+  newline +
+  newline + // Add one whole blank line
+  working +
+  newline + // data.labelWork + data.work
+  facebooking +
+  newline + // data.labelFacebook + data.facebook
+  penning +
+  newline + // data.labelCodepen + data.codepen
+  githubing +
+  newline + // data.labelGitHub + data.github
+  linkedining +
+  newline + // data.labelLinkedIn + data.linkedin
+  webing +
+  newline +
+  newline + // data.labelWeb + data.web
+  carding; // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(
+  path.join(__dirname, "bin/output"),
+  chalk.green(boxen(output, options))
+);
